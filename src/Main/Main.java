13bea0845dbf79a1f656f42577efa86297dcf20f
LopezS7 <samir@HPEnvyX360>
2023-09-17 19:08:27 -0600

    Primer commit de la calculadora Se inició creando la interfaz gráfica de la calculadora, con todos sus botones

---
+++ src/Main/Main.java
@@ -0,0 +1,8 @@
+package Main;
+
+/**
+ * @author samir
+ */
+public class Main {
+    
+}
---
+++ src/calculadora.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author samir
+ */
+public class calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
